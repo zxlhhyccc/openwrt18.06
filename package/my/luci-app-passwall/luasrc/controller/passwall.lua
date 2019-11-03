f26f5fea4ff098225f0c1e13087f629d359ebb51
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-11-03 15:22:19 +0800

    Update passwall.lua

--- package/my/luci-app-passwall/luasrc/controller/passwall.lua
+++ package/my/luci-app-passwall/luasrc/controller/passwall.lua
@@ -21,8 +21,9 @@ function index()
           _("Basic Settings"), 1).dependent = true
     entry({"admin", "vpn", "passwall", "server_list"},
           cbi("passwall/server_list"), _("Server List"), 2).dependent = true
+    entry({"admin", "vpn", "passwall", "subscription"},cbi("passwall/subscription"), _("Subscription Managenent"),4).leaf = true
     entry({"admin", "vpn", "passwall", "auto_switch"},
-          cbi("passwall/auto_switch"), _("Auto Switch"), 3).leaf = true
+          cbi("passwall/auto_switch"), _("Auto Switch"), 5).leaf = true
     entry({"admin", "vpn", "passwall", "other"}, cbi("passwall/other"),
           _("Other Settings"), 94).leaf = true
     if nixio.fs.access("/usr/sbin/haproxy") then
@@ -35,8 +36,9 @@ function index()
           _("Access control"), 97).leaf = true
     entry({"admin", "vpn", "passwall", "rulelist"}, cbi("passwall/rulelist"),
           _("Set Blacklist And Whitelist"), 98).leaf = true
+	 entry({"admin", "vpn", "passwall", "status"},form("passwall/status"),_("Status"), 99).leaf = true
     entry({"admin", "vpn", "passwall", "log"}, cbi("passwall/log"),
-          _("Watch Logs"), 99).leaf = true
+          _("Watch Logs"), 100).leaf = true
     entry({"admin", "vpn", "passwall", "serverconfig"},
           cbi("passwall/serverconfig")).leaf = true
 
